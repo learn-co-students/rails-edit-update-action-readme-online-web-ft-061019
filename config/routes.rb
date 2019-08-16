a96a1a85b92e45459cd240b3e81c91c643b310a7
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-16 15:04:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  # the above is the same as the below only that there is also a PUT route included.
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  # patch 'articles/:id', to: 'articles#update'
+
 end
